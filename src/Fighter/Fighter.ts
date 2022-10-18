12f703e73092c2aadb0779bd9d3081f5ba53730a
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-18 17:10:17 -0300

    feat: req 9

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,12 +1,10 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
-  lifePoints: number,
-  strength: number,
+export default interface Fighter extends SimpleFighter {
   defense: number,
   energy?: Energy,
-  attack(enemy: Fighter): void,
+  attack(enemy: Fighter | SimpleFighter): void,
   special?(enemy: Fighter): void,
   levelUp(): void,
-  receiveDamage(attackPoints: number): number,
 }
