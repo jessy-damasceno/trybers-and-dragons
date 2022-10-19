06f81d576645957d6073849bbbf630dada0d077e
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-19 15:09:22 -0300

    refatore: req 10

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -14,7 +14,7 @@ export default class PVP extends Battle {
   fight(): number {
     while (this.player1.lifePoints > 1 && this.player2.lifePoints > 1) {
       this.player1.attack(this.player2);
-      this.player1.receiveDamage(this.player2.strength);
+      this.player2.attack(this.player1);
     }
 
     return super.fight();
